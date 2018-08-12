852f18f97d6d8e8788a4c9e895bf720b1244320a
nefeithu <hxq_7324@qq.com>
2018-08-12 16:35:46 +0800

    配置从app分离

--- app.js
+++ app.js
@@ -7,13 +7,8 @@ var wechat = require('wechat');
 // 创建一个 express 实例
 const app = express();
 
-// 配置微信公众平台参数，在教程第二步中获取
-var config = {
-    token: 'your token', // 填第二步中获取的 `token`
-    appid: 'your appid', // 填第二步中获取的 `appid`
-    encodingAESKey: 'your encodingAESKey', // 填第二步中获取的 `encodingAESKey`
-    checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false 
-};
+// 载入配置文件
+var config = require('./config');
 
 app.use(express.query());
 
